f3181d3cf1a1b6267af9f5e6f2bb2647e4e3a645
Xiabbb <240944353@qq.com>
2022-10-08 01:05:00 +0800

    修改

--- 可视化学习.py
+++ 可视化学习.py
@@ -148,3 +148,17 @@ font="monospace"
 
 number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number from slider widget is:', number)
+
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.subheader('DataFrame')
+    st.write(df)
+    st.subheader('Descriptive Statistics')
+    st.write(df.describe())
+else:
+    st.info('☝️ Upload a CSV file')
