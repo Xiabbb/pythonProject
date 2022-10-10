c14986017932002d8e05d800475e68d4d42d0855
Xiabbb <240944353@qq.com>
2022-10-10 10:22:34 +0800

    修改

--- 可视化学习.py
+++ 可视化学习.py
@@ -1,8 +1,7 @@
-import pandas as pd
 import streamlit as st
 
 st.write('Hello world!')
 
-df_data = pd.read_excel('审核列表 (17).xlsx')
+# df_data = pd.read_excel('审核列表 (17).xlsx')
 
-st.dataframe(df_data)
+# st.dataframe(df_data)
